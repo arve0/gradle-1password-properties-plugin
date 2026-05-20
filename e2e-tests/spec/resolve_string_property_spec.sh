f79bf4a1cccb2c5983e2f2405a33f509a0220151
Arve Seljebu <arve.seljebu@webstep.no>
2026-05-20 13:17:25 +0200

    regression test for properties set with -P

--- e2e-tests/spec/resolve_string_property_spec.sh
+++ e2e-tests/spec/resolve_string_property_spec.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-Describe 'resolveProjectPropertyToString helper function'
+Describe 'plain string project property behavior'
   BeforeEach 'setup_fixture'
   AfterEach 'cleanup_fixture'
 
@@ -16,25 +16,18 @@ EOF
 
   BeforeEach 'create_op_mock'
 
-  write_helper_build() {
+  write_build_using_plugin_directly() {
     if [ -n "$LOCAL_MAVEN_REPO" ]; then
       plugin_decl='id("io.github.arve0.1password.properties") version "dev-SNAPSHOT"'
     else
       plugin_decl='id("io.github.arve0.1password.properties")'
     fi
     cat > "$FIXTURE_DIR/build.gradle.kts" <<EOF
-import org.gradle.api.provider.Provider
-
 plugins {
     $plugin_decl
 }
 
-fun resolveProjectPropertyToString(name: String): String {
-    val value = project.property(name)
-    return if (value is Provider<*>) value.get().toString() else value.toString()
-}
-
-val MY_PROP = resolveProjectPropertyToString("MY_PROP")
+val MY_PROP = project.property("MY_PROP")
 
 tasks.register("printProp") {
     doLast {
@@ -44,7 +37,7 @@ tasks.register("printProp") {
 EOF
   }
 
-  BeforeEach 'write_helper_build'
+  BeforeEach 'write_build_using_plugin_directly'
 
   It 'does not crash when property is a regular string'
     write_gradle_properties "MY_PROP=hello-world"
@@ -60,5 +53,14 @@ EOF
     When run run_gradle printProp
     The status should be success
     The output should include "MY_PROP=just-a-string"
+  End
+
+  It 'resolves property provided via -P as a plain string'
+    write_gradle_properties "onePassword.op.command=$OP_MOCK"
+
+    When run run_gradle printProp -PMY_PROP=cli-value
+    The status should be success
+    The output should include "MY_PROP=cli-value"
+    The output should not include "cannot be cast to class org.gradle.api.provider.Provider"
   End
 End
